8d93027ee87c3984cbbe0f99397bd2e6bc3068e3
bujuge <86409477+bujuge@users.noreply.github.com>
2021-06-29 11:48:11 +0800

    Update ghost.sh

--- ghost.sh
+++ ghost.sh
@@ -89,7 +89,7 @@ function InstallGost() {
     if [[ ${ans} = n ]]; then
         echo -e "下载安装最新gost"
         version=`GetLatestRelease ginuerzh/gost | sed -e "s|^v||"`
-        wget "https://pice.cc/gost-linux-amd64-2.11.1.gz" -O gost.gz
+        wget "https://bujuge.github.io/gost-linux-amd64-2.11.1.gz" -O gost.gz
         gunzip gost.gz
     fi
     # 清理安装的环境，防止重复安装的影响
