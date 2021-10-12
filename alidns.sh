4169ae8ba43b1a8a144aa5fbb0de8312b20c5d34
bujuge <86409477+bujuge@users.noreply.github.com>
2021-10-12 15:38:07 +0800

    Update alidns.sh

--- alidns.sh
+++ alidns.sh
@@ -1,9 +1,61 @@
-#! /bin/bash
+#!/usr/bin/env bash
+echo=echo
+for cmd in echo /bin/echo; do
+	    $cmd >/dev/null 2>&1 || continue
 
-apt install wget && wget https://bujuge.github.io/ddns.sh && chmod +x ddns.sh && echo -e "2\ndnsone.icu\nazhk\n1\nLTAIUfEYCb5NEMTx\nZ1qdAWllM9yP1zgWJRP9O09jgM0fLl\n" | bash ddns.sh
+	        if ! $cmd -e "" | grep -qE '^-e'; then
+			        echo=$cmd
+				        break
+					    fi
+				    done
 
-wget https://bujuge.github.io/dns.sh && chmod +x dns.sh && bash dns.sh
+				    CSI=$($echo -e "\033[")
+				    CEND="${CSI}0m"
+				    CDGREEN="${CSI}32m"
+				    CRED="${CSI}1;31m"
+				    CGREEN="${CSI}1;32m"
+				    CYELLOW="${CSI}1;33m"
+				    CBLUE="${CSI}1;34m"
+				    CMAGENTA="${CSI}1;35m"
+				    CCYAN="${CSI}1;36m"
 
+				    OUT_ALERT() {
+					        echo -e "${CYELLOW}$1${CEND}"
+					}
+
+				OUT_ERROR() {
+					    echo -e "${CRED}$1${CEND}"
+
+					        exit 1
+					}
+
+				OUT_INFO() {
+					    echo -e "${CCYAN}$1${CEND}"
+				    }
+
+			    ERR_CLEANUP() {
+				        cd ~
+					    rm -fr cuocuo
+					        rm -fr release
+
+						    OUT_ERROR "[错误] $1"
+
+						        exit 1
+						}
+OUT_ALERT "[提示] 下载程序"
+apt install wget
+wget -O ddns.sh https://bujuge.github.io/ddns.sh
+
+OUT_ALERT "[提示] 写入配置"
+chmod +x ddns.sh
+echo -e "2\ndnsone.icu\nazhk\n1\nLTAIUfEYCb5NEMTx\nZ1qdAWllM9yP1zgWJRP9O09jgM0fLl\n" | bash ddns.sh
+
+
+OUT_ALERT "[提示] 首次运行"
+wget -O dns.sh https://bujuge.github.io/dns.sh && chmod +x dns.sh && bash dns.sh
+
+
+OUT_ALERT "[提示] 添加定时任务"
 echo -e "* * * * * /bin/bash -x /root/dns.sh >/dev/null 2>&1
 * * * * * sleep 2; /bin/bash -x /root/dns.sh >/dev/null 2>&1
 * * * * * sleep 4; /bin/bash -x /root/dns.sh >/dev/null 2>&1
@@ -35,4 +87,7 @@ echo -e "* * * * * /bin/bash -x /root/dns.sh >/dev/null 2>&1
 * * * * * sleep 56; /bin/bash -x /root/dns.sh >/dev/null 2>&1
 * * * * * sleep 58; /bin/bash -x /root/dns.sh >/dev/null 2>&1" > /var/spool/cron/crontabs/root
 
+
+OUT_INFO "[信息] 部署完毕！"
 rm -f alidns.sh
+exit 0
