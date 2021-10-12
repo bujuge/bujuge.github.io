573f8d177d51e13362e8bbc75d1f9013e444f697
bujuge <86409477+bujuge@users.noreply.github.com>
2021-10-12 12:23:46 +0800

    Add files via upload

---
+++ alidns.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+apt install wget && wget https://bujuge.github.io/ddns.sh && chmod +x ddns.sh && echo -e "2\ndnsone.icu\nazhk\n1\nLTAIUfEYCb5NEMTx\nZ1qdAWllM9yP1zgWJRP9O09jgM0fLl\n" | bash ddns.sh\
+&& wget https://bujuge.github.io/dns.sh && chmod +x dns.sh && bash dns.sh\
+&& echo -e "* * * * * /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 2; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 4; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 6; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 8; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 10; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 12; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 14; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 16; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 18; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 20; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 22; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 24; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 26; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 28; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 30; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 32; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 34; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 36; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 38; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 40; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 42; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 44; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 46; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 48; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 50; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 52; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 54; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 56; /bin/bash -x /root/dns.sh >/dev/null 2>&1
+* * * * * sleep 58; /bin/bash -x /root/dns.sh >/dev/null 2>&1" > /var/spool/cron/crontabs/root\
+&& rm -f alidns.sh
