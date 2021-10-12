9373b9cfeb5991a0479d861da3920253f8fe895c
bujuge <86409477+bujuge@users.noreply.github.com>
2021-10-12 11:45:01 +0800

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -2,7 +2,7 @@
 
 ping -w 3 azhk.dnsone.icu -c 1 >/dev/null 2>&1 #执行一次会生成一个以errtmp开头的文件，可用/dev/null代替，不生成任何文件
 if [ $? = 0 ] ; then
-echo "OK!"
+echo "AZHK is GOOD!"
 else
 echo -e "3\n" | bash ddns.sh
 fi
