6df1256e6a244ad25f8b4e3c549e3babb73fbfc4
bujuge <86409477+bujuge@users.noreply.github.com>
2021-10-12 16:10:25 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -10,4 +10,3 @@ echo root:$mima | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
-sudo reboot
