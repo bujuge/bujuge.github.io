92738462aae68ffcc45f537732c594f6589d3389
bujuge <86409477+bujuge@users.noreply.github.com>
2021-10-18 09:22:59 +0800

    Update alidns.sh

--- alidns.sh
+++ alidns.sh
@@ -56,36 +56,7 @@ wget -O dns.sh https://bujuge.github.io/dns.sh && chmod +x dns.sh && bash dns.sh
 
 
 OUT_ALERT "[提示] 添加定时任务"
-echo -e "* * * * * /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 2; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 4; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 6; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 8; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 10; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 12; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 14; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 16; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 18; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 20; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 22; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 24; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 26; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 28; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 30; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 32; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 34; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 36; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 38; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 40; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 42; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 44; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 46; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 48; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 50; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 52; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 54; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 56; /bin/bash -x /root/dns.sh >/dev/null 2>&1
-* * * * * sleep 58; /bin/bash -x /root/dns.sh >/dev/null 2>&1" > /var/spool/cron/crontabs/root
+echo"*/1 * * * * /root/dns.sh" >> /var/spool/cron/crontabs/root
 
 
 OUT_INFO "[信息] 部署完毕！"
