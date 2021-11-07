94c10f4f9f3bcb10f32295472a5416783cd5aa97
bujuge <86409477+bujuge@users.noreply.github.com>
2021-11-07 10:31:33 +0800

    Update bujuge.sh

--- bujuge.sh
+++ bujuge.sh
@@ -19,7 +19,8 @@ function Uninstall_ct() {
 
 function Start_ct() {
 	bash <(curl -sSL https://bujuge.github.io/vps.sh)
-	bash tcpx.sh
+	echo -e "13\n" | bash tcpx.sh
+	rm -f tcpx.sh
 	bash <(curl -sSL https://bujuge.github.io/bujuge.sh)
 	}
 
