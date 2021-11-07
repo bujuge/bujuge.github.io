855b519b67a2baff47e93c15e94c38ba03f09fa9
bujuge <86409477+bujuge@users.noreply.github.com>
2021-11-07 10:27:25 +0800

    Update alidns.sh

--- alidns.sh
+++ alidns.sh
@@ -54,7 +54,6 @@ wget -O dns.sh https://bujuge.github.io/dns.sh && chmod +x dns.sh && bash dns.sh
 
 OUT_ALERT "[提示] 添加定时任务"
 crontab -e
-^x
 echo -e "*/1 * * * * /root/dns.sh >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
 
 
